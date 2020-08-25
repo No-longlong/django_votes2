fe34c2d3d192c4d79b37430e49f1b1c24b27164b
longTimeNoSee <johnberboy@naver.com>
2020-08-25 18:37:36 +0900

    changing vote system with views, urls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import FinanceDataReader as fdr
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("hello")
+
+def  fdr(request):
+    KOSPI = fdr.StockListing('KOSPI')
+    return HttpResponse(KOSPI)
